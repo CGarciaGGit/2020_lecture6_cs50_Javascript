cc7e498610139ef58d63cbfed69315471213a2ad
CGarciaGGit <carlos_a_garcia_garza@hotmail.com>
2021-10-29 01:53:17 -0500

    update lesson 6 finished

--- counter.js
+++ counter.js
@@ -1,20 +1,33 @@
 // JavaScript source code
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
 
-let counter = 0;
+}
+
+//let counter = 0;
+
 
 function count() {
-    const counter1 = document.querySelector('h2');
-    counter = counter + 1;
-    counter += 1;
+    let counter = localStorage.getItem('counter');
     counter++;
-    counter1.innerHTML = counter;
+    document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
 
-    if (counter % 10 === 0) {
-        alert(`Count is now ${counter}`);
-    }
+  //  const counter1 = document.querySelector('h2');
+  //  counter = counter + 1;
+  //  counter += 1;
+  //  counter++;
+  //  counter1.innerHTML = counter;
+
+   // if (counter % 10 === 0) {
+   //     alert(`Count is now ${counter}`);
+   // }
 
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
+
+//    setInterval(count, 1000);
 });
